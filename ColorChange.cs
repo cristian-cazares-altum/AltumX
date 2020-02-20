924e2a572218e0c84a3bc9e2a6ae37131c3fbd72
cristian-cazares-altum <60044033+cristian-cazares-altum@users.noreply.github.com>
2020-02-20 12:50:33 -0700

    ColoChange
    Ese código (script), servirá para cambiar los colores de un cubo que lo contenga. Los colores son ajustables.

---
+++ ColorChange.cs
@@ -0,0 +1,32 @@
+﻿/*
+========================================================
+                        Script By:
+                    Cristian Cázares
+=========================================================
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    //Se declaran los tres colores que se van a usar
+    public Color enterColor;
+    public Color exitColor;
+    public Color clickColor;
+
+
+    //Se asignan dichos colores
+    public void Enter()
+    {
+        GetComponent<Renderer>().material.color = enterColor;
+    }
+    public void Exit()
+    {
+        GetComponent<Renderer>().material.color = exitColor;
+    }
+    public void Click()
+    {
+        GetComponent<Renderer>().material.color = clickColor;
+    }
+}
