354344f5bb4b76c05f0f5fbff7b85155d2c6d2ce
cristian-cazares-altum <60044033+cristian-cazares-altum@users.noreply.github.com>
2020-02-22 10:08:31 -0700

    Version 2

---
+++ ColorChangeV2.cs
@@ -0,0 +1,37 @@
+﻿/*
+========================================================
+                        Script By:
+                    Cristian Cázares
+=========================================================
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeV2 : MonoBehaviour
+{
+    //Se declaran los tres colores que se van a usar
+    public Color enterColor;
+    Color exitColor;
+    public Color clickColor;
+
+    void Start()
+    {
+        exitColor = GetComponent<Renderer>().material.color;
+    }
+
+
+    //Se asignan dichos colores
+    public void Enter()
+    {
+        GetComponent<Renderer>().material.color = enterColor;
+    }
+    public void Exit()
+    {
+        GetComponent<Renderer>().material.color = exitColor;
+    }
+    public void Click()
+    {
+        GetComponent<Renderer>().material.color = clickColor;
+    }
+}
